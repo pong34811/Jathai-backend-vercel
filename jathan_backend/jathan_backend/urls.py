8677114087975795135ae1ac67c20d02ee3fe0c3
Kanya Lenovo <pong34811@gmail.com>
2025-02-16 00:59:20 +0700

    Add API routing for boards, lists, and tasks; integrate authentication views

--- jathan_backend/jathan_backend/urls.py
+++ jathan_backend/jathan_backend/urls.py
@@ -1,22 +1,26 @@
-"""
-URL configuration for jathan_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+from accounts.views import GoogleLogin, email_confirmation, reset_password_confirm, UserDetailView, LineCallbackView
+from rest_framework import routers
+from boards.views import BoardViewSet, ListViewSet, TaskViewSet
+
+# ตั้งค่า DefaultRouter สำหรับ API views ของเรา
+router = routers.DefaultRouter()
+router.register(r'boards', BoardViewSet, basename='boards')
+router.register(r'lists', ListViewSet, basename='lists')
+router.register(r'tasks', TaskViewSet, basename='tasks')
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/account-confirm-email/<str:key>/', email_confirmation),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('reset/password/confirm/<int:uid>/<str:token>', reset_password_confirm, name="password_reset_confirm"),
+    path('dj-rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
+    path('user/<int:pk>/', UserDetailView.as_view(), name='user-detail'),  # รองรับ /user/1/
+    path('line/callback/', LineCallbackView.as_view(), name='line-callback'),
 ]
+
+# urlpatterns += [re_path(f'^.*', TemplateView.as_view(template_name="index.html"))]
